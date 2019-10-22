3aa4a0590617f468e58afa1056aa2b52c9051a11
Noel Lopes <noel@ipg.pt>
2019-10-22 12:04:15 +0100

    Add a service to obtain a data repository

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -24,6 +25,7 @@ namespace SportsStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<ISportsStoreRepository, FakeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
